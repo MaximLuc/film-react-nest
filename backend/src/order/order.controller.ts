173e4af39b40156a79be72428eeb3d0e1b59af39
MaximLuc <maximhop200307@gmail.com>
2026-02-28 20:58:33 +0300

    add:services,controllers, and repository

---
+++ backend/src/order/order.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Body, Post } from '@nestjs/common';
+import{OrderDto} from './dto/order.dto'
+import { OrderService } from './order.service';
+
+@Controller('order')
+export class OrderController {
+    constructor(private readonly ordersService:OrderService){}
+    
+    
+    @Post()
+    create(@Body() dto: OrderDto){
+        return this.ordersService.createOrder(dto)
+    }
+}
---
+++ backend/src/order/order.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { FilmsRepository } from 'src/repository/films.repository';
+import {TicketDto, OrderDto,OrderResultDto} from './dto/order.dto'
+
+type ApiListResponse<T> = {total:number;items:T[]};
+
+@Injectable()
+export class OrderService {
+    constructor(private readonly repo:FilmsRepository){}
+
+    async createOrder(dto:OrderDto): Promise<ApiListResponse<OrderResultDto>>{
+        const results: OrderResultDto[] = [];
+
+        for (const t of dto.tickets){
+            const place = `${t.row}:${t.seat}`;
+            const session = await this.repo.getSitsInfo(t.film,t.session)
+
+            if((session.taken ?? []).includes(place)){
+                throw new BadRequestException('Seat already taken');
+            }
+            await this.repo.reservePlace(t.film,t.session,place);
+
+            results.push({
+                id:crypto.randomUUID(),
+                film: t.film,
+                session: t.session,
+                daytime: session.daytime,
+                row: t.row,
+                seat: t.seat,
+                price: session.price,
+            });
+        }
+        return {total:results.length,items:results}
+    }
+}
